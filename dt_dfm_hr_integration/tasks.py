c16dba485bb65bd12def918c32e6b5036c3bbc55
Dev1-dipanetech <dev1@dipanetech.com>
2023-12-19 10:40:06 +0530

    updated tasks.py

--- dt_dfm_hr_integration/tasks.py
+++ dt_dfm_hr_integration/tasks.py
@@ -145,100 +145,97 @@ def all():
 
 
 
-
 def cron():
     date = frappe.utils.now_datetime()
     today_date = date.date().day
 
-    if (today_date == 1) or (today_date == 2) or (today_date == 3):
-        print("\n\n\nDFM HR Integration Process Start\n\n\n")
+    if 1 <= today_date <= 3:
+        try:
+            frappe.logger.info("DFM HR Integration Process Start")
 
-        settings = frappe.get_single("DFM HR Settings")
+            settings = frappe.get_single("DFM HR Settings")
 
-        server_address = settings.sftp_server_address
-        user = settings.sftp_user
-        password = settings.get_password('sftp_password')
-        port = settings.sftp_port
+            server_address = settings.sftp_server_address
+            user = settings.sftp_user
+            password = settings.get_password('sftp_password')
+            port = settings.sftp_port
 
-        folder = settings.sftp_folder
+            folder = settings.sftp_folder
 
-        # Establish an SFTP connection
-        cnopts = pysftp.CnOpts()
-        cnopts.hostkeys = None  # Disable host key checking (use with caution)
-        with pysftp.Connection(server_address, port=port, username=user, password=password, cnopts=cnopts) as sftp:
-            if folder:
-                sftp.chdir(folder)
-                file_list = sftp.listdir()
-            else:
-                file_list = sftp.listdir()
+            cnopts = pysftp.CnOpts()
+            cnopts.hostkeys = None  # Disable host key checking (use with caution)
 
-            xlsx_files = [file for file in file_list if file.lower().endswith(".xlsx")]
+            with pysftp.Connection(server_address, port=port, username=user, password=password, cnopts=cnopts) as sftp:
+                if folder:
+                    sftp.chdir(folder)
+                    file_list = sftp.listdir()
+                else:
+                    file_list = sftp.listdir()
 
-            existing_file_names = frappe.get_all("DFM HR Log", filters={}, fields=["file_name"])
-            existing_file_names = [file["file_name"] for file in existing_file_names]
+                xlsx_files = [file for file in file_list if file.lower().endswith(".xlsx")]
 
-            for file_name in xlsx_files:
-                if file_name in existing_file_names:
-                    print("File name {} already exists in DFM HR Log. Skipping file...".format(file_name))
-                    continue
+                existing_file_names = frappe.get_all("DFM HR Log", filters={}, fields=["file_name"])
+                existing_file_names = [file["file_name"] for file in existing_file_names]
 
-                try:
-                    # Retrieve the content as bytes
-                    file_content = sftp.get(file_name)
+                for file_name in xlsx_files:
+                    if file_name in existing_file_names:
+                        frappe.logger.info("File name {} already exists in DFM HR Log. Skipping file...".format(file_name))
+                        continue
 
-                    # Use BytesIO for the content
-                    file_content_io = BytesIO(file_content)
+                    try:
+                        # Retrieve the content as bytes
+                        file_content = sftp.get(file_name)
 
-                    print("Processing file: {}".format(file_name))
+                        # Use BytesIO for the content
+                        file_content_io = BytesIO(file_content)
 
-                    workbook = openpyxl.load_workbook(file_content_io)
-                    sheet = workbook.active
+                        frappe.logger.info("Processing file: {}".format(file_name))
 
-                    file_doc = None
+                        workbook = openpyxl.load_workbook(file_content_io)
+                        sheet = workbook.active
 
-                    if not frappe.get_all("File", filters={"file_name": file_name}):
-                        file_content_str = save_virtual_workbook(workbook)
-                        file_doc = frappe.get_doc({
-                            "doctype": "File",
-                            "file_name": file_name,
-                            "content": file_content_str,
-                            "is_private": 1,
-                            "folder": "Home"
-                        })
-                        file_doc.insert(ignore_permissions=True)
-                    else:
-                        file_doc = frappe.get_doc("File", {"file_name": file_name})
+                        file_doc = None
 
-                    header_row = [cell.value for cell in sheet[2]]
-                    print("Header Row: {}".format(header_row))
+                        if not frappe.get_all("File", filters={"file_name": file_name}):
+                            file_content_str = save_virtual_workbook(workbook)
+                            file_doc = frappe.get_doc({
+                                "doctype": "File",
+                                "file_name": file_name,
+                                "content": file_content_str,
+                                "is_private": 1,
+                                "folder": "Home"
+                            })
+                            file_doc.insert(ignore_permissions=True)
+                        else:
+                            file_doc = frappe.get_doc("File", {"file_name": file_name})
 
-                    grouped_rows = {}
-                    for row_number, row in enumerate(sheet.iter_rows(min_row=3, values_only=True), start=3):
-                        if not all(cell is None for cell in row):
-                            companies_value = row[header_row.index('Companies')]
-                            if companies_value not in grouped_rows:
-                                grouped_rows[companies_value] = []
-                            grouped_rows[companies_value].append((row_number, row))
+                        header_row = [cell.value for cell in sheet[2]]
+                        frappe.logger.info("Header Row: {}".format(header_row))
 
-                    # Print the grouped rows
-                    for companies_value, rows in grouped_rows.items():
-                        create_salary_register_entry(rows, header_row, companies_value, file_name, file_doc)
-                        # print(f"Rows with Companies value '{companies_value}':")
-                        # print(rows)
+                        grouped_rows = {}
+                        for row_number, row in enumerate(sheet.iter_rows(min_row=3, values_only=True), start=3):
+                            if not all(cell is None for cell in row):
+                                companies_value = row[header_row.index('Companies')]
+                                if companies_value not in grouped_rows:
+                                    grouped_rows[companies_value] = []
+                                grouped_rows[companies_value].append((row_number, row))
 
-                        # for row_number, row in rows:
-                        #     print(f"  Row {row_number}: {row}")
+                        for companies_value, rows in grouped_rows.items():
+                            create_salary_register_entry(rows, header_row, companies_value, file_name, file_doc)
 
-                except Exception as e:
-                    log_error(file_name, "", "", str(e))
-                    continue
-        # sftp.close()
-        print("\n\n\n\n\nAll files have been downloaded and processed.")
-        frappe.msgprint("Successfully synced data from SFTP.")
+                    except Exception as e:
+                        log_error(file_name, "", "", str(e))
+                        continue
+
+            frappe.logger.info("All files have been downloaded and processed.")
+            frappe.msgprint("Successfully synced data from SFTP.")
+
+        except Exception as e:
+            log_error("", "", "", str(e))
+            frappe.msgprint("Error occurred during SFTP sync. Check logs for details.")
 
     else:
         pass
-
 
 
 
